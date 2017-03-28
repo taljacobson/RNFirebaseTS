b16939f17350da70d5909c969546022589ff0caf
taljacobson <tal1989@gmail.com>
2017-03-28 20:37:18 +0300

    add anaytics button

--- src/index.android.tsx
+++ src/index.android.tsx
@@ -1,40 +1,120 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-
-import HelloWorld from "./components/HelloWorld/index";
+import { View, Text, StyleSheet, ViewStyle } from "react-native";
+import Button from 'react-native-button';
+import LoginForm from './components/LoginForm';
 import RNFirebase from 'react-native-firebase';
 
 const firebase = new RNFirebase();
-firebase.on('debug', (msg: any) => console.log('Receiveds debug message', msg))
-console.log("any")
+// firebase.on('debug', bug => console.log(bug))
 interface Props {
 
 }
 
 interface State {
-
+  user?: any;
+  loggedIn: boolean;
+  clickedCount?: number
 }
 
 export default class App extends Component<Props, State> {
+  state: State = {
+    user: null,
+    loggedIn: false
+  }
+
   componentWillMount() {
+
     firebase.analytics().setAnalyticsCollectionEnabled(true);
     firebase.analytics().setCurrentScreen('user_profile');
-    firebase.analytics().logEvent('clicked_advert', { id: 1337 });
-  }
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-                </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-                </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-                </Text>
 
-        <HelloWorld style={styles.helloworld} max={10} />
+    firebase.auth().onAuthStateChanged((user: any) => {
+      if (user) {
+
+        console.log('onAuthStateChanged', user)
+        const { uid } = user
+        firebase.analytics().setUserId(uid)
+        this.setState({ user, loggedIn: !this.state.loggedIn })
+      } else {
+        this.setState({
+          user: null,
+          loggedIn: false
+        })
+      }
+
+    }, (err) => console.log(err))
+  }
+
+  componentDidMount() {
+
+  }
+  renderAuth() {
+    if (!this.state.loggedIn || !this.state.user)
+      return <LoginForm />;
+    return (
+      <Button onPress={() => firebase.auth().signOut()}>
+        Logout
+      </Button>
+    )
+  }
+
+  renderIncrease() {
+    if (this.state.loggedIn && this.state.user) {
+      let uid: string = ''
+      if (firebase.auth().currentUser) {
+        uid = firebase.auth().currentUser.uid;
+        firebase.database().ref('user/').child(uid).on('value', (snapshot) => {
+          let val = snapshot.val()
+          if (val) {
+            console.log(val)
+            const clickedCount = val.clicked
+            if (!(this.state.clickedCount === clickedCount))
+              this.setState({ clickedCount })
+          }
+        })
+      }
+    }
+
+    return (
+      <View>
+
+        <Button onPress={this.Analytic.bind(this)}>
+          increase + {this.state.clickedCount}
+        </Button>
+
+      </View>)
+  }
+
+  increaseCount() {
+    const uid = firebase.auth().currentUser.uid;
+    firebase.database().ref('user/').child(uid).update({ clicked: this.state.clickedCount + 1 })
+    this.setState({})
+  }
+
+  Analytic() {
+    if (this.state.user) {
+
+      firebase.analytics().logEvent('clicked_advert', { uid: this.state.user.uid });
+    }
+  }
+
+  render() {
+
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            Welcome to React Native!
+                </Text>
+          <Text style={styles.instructions}>
+            To get started, edit index.android.js
+                </Text>
+          <Text style={styles.instructions}>
+            Shake or press menu button for dev menu
+                </Text>
+          {this.state.loggedIn ? this.renderIncrease() : null}
+
+        </View>
+        {this.renderAuth()}
       </View>
     );
   }
@@ -46,21 +126,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F5FCFF",
-  } as React.ViewStyle,
+  } as ViewStyle,
 
   welcome: {
     fontSize: 20,
     textAlign: "center",
     margin: 10,
-  } as React.TextStyle,
+  } as ViewStyle,
 
   instructions: {
     textAlign: "center",
     color: "#333333",
     marginBottom: 5,
-  } as React.TextStyle,
+  } as ViewStyle,
 
   helloworld: {
     marginVertical: 15,
-  } as React.ViewStyle,
+  } as ViewStyle,
 });
